71338ce5a481c6ad20c296a6db5c96bec9c3edf6
DimitriMumladze <kenwo.dm@gmail.com>
2026-04-23 18:17:49 +0400

    feat(models): add Patient and Gender entity classes
    - Gender: GenderID, GenderName + ToString() override for ComboBox binding
    - Patient: ID, FullName, Dob, GenderID, GenderName, Phone, Address

---
+++ MedSoftPatients/Models/Gender.vb
@@ -0,0 +1,9 @@
+﻿Public Class Gender
+    Public Property GenderId As Integer
+    Public Property GenderName As String
+
+    Public Overrides Function ToString() As String
+        Return GenderName
+    End Function
+
+End Class
---
+++ MedSoftPatients/Models/Patienet.vb
@@ -0,0 +1,10 @@
+﻿Public Class Patienet
+    Public Property ID As Integer
+    Public Property FullName As String
+    Public Property Dob As Date
+    Public Property GenderID As Integer
+    Public Property GenderName As String
+    Public Property Phone As String
+    Public Property Address As String
+
+End Class
