b4415490fecce60d4957d8a0aad9239ddc6e62d3
DimitriMumladze <kenwo.dm@gmail.com>
2026-04-23 19:22:22 +0400

    feat(ui): design FrmMain layout with buttons and grid
    - Rename Form1 to FrmMain, move to Forms/ folder
    - Set Georgian title and Sylfaen font
    - Add top panel with 3 buttons: Add (green), Edit, Delete (red)
    - Add DataGridView (dgvPatients) docked fill, read-only, full-row select
    - Configure grid to disallow inline add/edit/delete
    - Added Logo Folder and pic

---
+++ MedSoftPatients/Forms/FrmMain.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmMain))
+        Me.pnlTop = New System.Windows.Forms.Panel()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.dgvPatients = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvPatients, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pnlTop
+        '
+        Me.pnlTop.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnlTop.Location = New System.Drawing.Point(0, 0)
+        Me.pnlTop.Name = "pnlTop"
+        Me.pnlTop.Size = New System.Drawing.Size(984, 50)
+        Me.pnlTop.TabIndex = 0
+        '
+        'btnAdd
+        '
+        Me.btnAdd.ForeColor = System.Drawing.Color.Green
+        Me.btnAdd.Location = New System.Drawing.Point(10, 10)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(130, 32)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "➕ დამატება"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnEdit
+        '
+        Me.btnEdit.ForeColor = System.Drawing.Color.Olive
+        Me.btnEdit.Location = New System.Drawing.Point(150, 10)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(150, 32)
+        Me.btnEdit.TabIndex = 2
+        Me.btnEdit.Text = "✏️ რედაქტირება"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.ForeColor = System.Drawing.Color.Red
+        Me.btnDelete.Location = New System.Drawing.Point(310, 10)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(130, 32)
+        Me.btnDelete.TabIndex = 3
+        Me.btnDelete.Text = "✖️ წაშლა"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'dgvPatients
+        '
+        Me.dgvPatients.AllowUserToAddRows = False
+        Me.dgvPatients.AllowUserToDeleteRows = False
+        Me.dgvPatients.AllowUserToResizeColumns = False
+        Me.dgvPatients.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgvPatients.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.dgvPatients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvPatients.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvPatients.Location = New System.Drawing.Point(0, 50)
+        Me.dgvPatients.MultiSelect = False
+        Me.dgvPatients.Name = "dgvPatients"
+        Me.dgvPatients.ReadOnly = True
+        Me.dgvPatients.RowHeadersVisible = False
+        Me.dgvPatients.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvPatients.Size = New System.Drawing.Size(984, 589)
+        Me.dgvPatients.TabIndex = 4
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ClientSize = New System.Drawing.Size(984, 639)
+        Me.Controls.Add(Me.dgvPatients)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnEdit)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.pnlTop)
+        Me.Font = New System.Drawing.Font("Sylfaen", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.MinimumSize = New System.Drawing.Size(800, 500)
+        Me.Name = "FrmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MedSoft - Patient Management"
+        CType(Me.dgvPatients, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlTop As Panel
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents dgvPatients As DataGridView
+End Class
---
+++ MedSoftPatients/Forms/FrmMain.vb
@@ -0,0 +1,9 @@
+﻿Public Class FrmMain
+    Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub dgvPatients_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvPatients.CellContentClick
+
+    End Sub
+End Class
